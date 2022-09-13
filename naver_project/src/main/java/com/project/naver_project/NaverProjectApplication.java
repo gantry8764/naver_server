6013bcf0ce719455e0217803a5784cd248e391e2
gantry8764 <gantry8764@gmail.com>
2022-09-13 14:52:09 +0900

    start project

---
+++ naver_project/src/main/java/com/project/naver_project/NaverProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.naver_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverProjectApplication.class, args);
+	}
+
+}
---
+++ naver_project/src/test/java/com/project/naver_project/NaverProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.naver_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
